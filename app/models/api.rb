a0fcd0cc69d842d30c111ec9e5aedb986ae017f5
Hilary Barr <hilaryrosebarr@gmail.com>
2014-08-10 22:22:29 -0400

    api key

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,9 +1,13 @@
 class API
 	# API_KEY = File.read('lib/assets/.api_key')
 	# API_SECRET = File.read('lib/assets/.api_secret')
+	 $translator = MicrosoftTranslator::Client.establish_connection!(
+   	:API_KEY   => ENV['API_KEY'],
+		:API_SECRET => ENV['API_SECRET']
+	 )
 
 	def self.call_api(text_to_translate, lang)
- 		$translator = MicrosoftTranslator::Client.new(API_KEY, API_SECRET)
+ 		 #$translator = MicrosoftTranslator::Client.new('translate-yo', API_SECRET)
  		translation = $translator.translate(text_to_translate, "en", lang.code, "text/html")
  	end
 
